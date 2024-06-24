8d048b7e5f0d2e5a0278334e7ebac25c13ea166c
HumbertoJim <humbertodjimz@gmail.com>
2024-06-23 21:56:44 -0500

    Add Character Interactions to DialgueData

--- Assets/HJimDev/DialogueManagement/Base/Scripts/Data/DialogueData.cs
+++ Assets/HJimDev/DialogueManagement/Base/Scripts/Data/DialogueData.cs
@@ -12,39 +12,73 @@ namespace DialogueManagement
         {
             public class DialogueData : DictionaryCollectionManager
             {
+                const string CHOICES_KEY = "Choices";
+                const string FLAGS_KEY = "Flags";
+                const string INTERACTIONS_KEY = "Interactions";
+                const string RELATIONSHIPS_KEY = "Relationships";
+
                 protected override void Awake()
                 {
                     Awake("Dialogue");
                 }
 
+                // Choices
                 public int GetChoice(string choiceID)
                 {
-                    return Serializer.GetDataAsInt("Choices", choiceID);
+                    return Serializer.GetDataAsInt(CHOICES_KEY, choiceID);
                 }
 
                 public void SetChoice(string choiceID, int value)
                 {
-                    Serializer.SetData("Choices", choiceID, value.ToString());
+                    Serializer.SetData(CHOICES_KEY, choiceID, value.ToString());
                 }
 
                 public bool ChoiceExists(string choiceID)
                 {
-                    return Serializer.DataExists("Choices", choiceID);
+                    return Serializer.DataExists(CHOICES_KEY, choiceID);
                 }
 
+                // Flags
                 public bool GetFlag(string flagID)
                 {
-                    return Serializer.GetDataAsBool("Flags", flagID);
+                    return Serializer.GetDataAsBool(FLAGS_KEY, flagID);
                 }
 
                 public void SetFlag(string flagID, bool value)
                 {
-                    Serializer.SetData("Flags", flagID, value);
+                    Serializer.SetData(FLAGS_KEY, flagID, value);
                 }
 
                 public bool FlagExists(string flagID)
                 {
-                    return Serializer.DataExists("Flags", flagID);
+                    return Serializer.DataExists(FLAGS_KEY, flagID);
+                }
+
+                // Character Interactions
+                public int CountInteractions(string characterID)
+                {
+                    return Serializer.GetDataAsInt(INTERACTIONS_KEY, characterID);
+                }
+
+                public void RegisterInteraction(string characterID)
+                {
+                    Serializer.SetData(INTERACTIONS_KEY, characterID, CountInteractions(characterID) + 1);
+                }
+
+                // Character Relationships
+                public int GetRelationship(string characterID)
+                {
+                    return Serializer.GetDataAsInt(RELATIONSHIPS_KEY, characterID);
+                }
+
+                public void ImproveRelationship(string characterID, int value)
+                {
+                    Serializer.SetData(RELATIONSHIPS_KEY, characterID, GetRelationship(characterID) + value);
+                }
+
+                public void DecreaseRelationship(string characterID, int value)
+                {
+                    Serializer.SetData(RELATIONSHIPS_KEY, characterID, GetRelationship(characterID) - value);
                 }
             }
         }
---
+++ Assets/HJimDev/DialogueManagement/Base/Scripts/Data/DialoguerData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataManagement.Managers;
+
+
+namespace DialogueManagement
+{
+    namespace Base
+    {
+        namespace Data
+        {
+            public class DialoguerData
+            {
+
+            }
+        }
+    }
+}
